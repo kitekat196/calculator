5cc7159abe65bf68ff2d5cb534f140d3628126c6
kitekat196 <47691012+kitekat196@users.noreply.github.com>
2022-04-23 09:17:09 +0300

    Update Test.java

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -13,7 +13,9 @@ public class Test {
             if (Character.isDigit(numbers[0].charAt(0)) && Character.isDigit(numbers[2].charAt(0))) {
                 int first = Integer.parseInt(numbers[0]);
                 int second = Integer.parseInt(numbers[2]);
-
+                if((first > 10 || first < 0)|| (second > 10 || second < 0)){
+                    throw new Exception("одно из чисел больше 10 или меньше 0");
+                }
                 System.out.println(result(numbers[1], first, second));
             } else if (isRoman(numbers)) {
                 int result = result(numbers[1], (convertRomanToNum(numbers[0])), convertRomanToNum(numbers[2]));
@@ -34,16 +36,15 @@ public class Test {
     }
 
     public static boolean isRoman(String[] numbers) {
-        if ((numbers[0].charAt(0) == 'I' || numbers[0].charAt(0) == 'V' || numbers[0].charAt(0) == 'X') &&
-                (numbers[2].charAt(0) == 'I' || numbers[2].charAt(0) == 'V' || numbers[2].charAt(0) == 'X'))
-        {
+        if ((numbers[0].charAt(0) == 'I' || numbers[0].charAt(0) == 'V' || numbers[0].charAt(0) == 'X' || numbers[0].charAt(0) == 'L' || numbers[0].charAt(0) == 'C') &&
+                (numbers[2].charAt(0) == 'I' || numbers[2].charAt(0) == 'V' || numbers[2].charAt(0) == 'X' || numbers[2].charAt(0) == 'L' || numbers[2].charAt(0) == 'C')) {
             return true;
         } else {
             return false;
         }
     }
 
-    public static String convertNumToRoman(int num) {
+    public static String convertNumToRoman(int num){
         String[] roman = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                 "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
                 "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
@@ -53,20 +54,14 @@ public class Test {
         return roman[num];
     }
 
-    public static int convertRomanToNum(String str) {
-        String[] roman = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
-                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
-                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
-                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
-                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+    public static int convertRomanToNum(String str) throws Exception {
+        String[] roman = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         for (int i = 0; i < roman.length; i++) {
             if (roman[i].equals(str)) {
                 return i;
             }
         }
-
-        return 0;
+        throw new Exception ("одно из чисел больше 10 или меньше 0");
     }
 
     public static int result(String str, int first, int second) {
